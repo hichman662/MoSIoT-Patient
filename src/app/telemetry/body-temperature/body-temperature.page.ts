dd1de1da5740e66fe2f97be03f5ea594417e6863
Seyed Shahabadin Nasabeh <hichman662@gmail.com>
2022-07-04 12:44:19 +0200

    All Telemetries

--- src/app/telemetry/body-temperature/body-temperature.page.ts
+++ src/app/telemetry/body-temperature/body-temperature.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable max-len */
+import { ActivatedRoute, Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-body-temperature',
@@ -7,9 +13,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyTemperaturePage implements OnInit {
 
-  constructor() { }
+  @ViewChild('barCanvas') private barCanvas: ElementRef;
+  public bodyTemperature: any;
+  barChart: any;
+  label: any [] =[' 2022/5/22', '2022/5/23', '2022/5/26', '2022/5/27', '2022/6/01','2022/6/2'];
+  data: any[]=[37, 37.5, 36, 37.5, 36.5, 37,5 ];
+  date= new Date();
+  constructor(  public router: Router,
+    private route: ActivatedRoute,
+    private storage: Storage
+    ) { Chart.register(...registerables);}
 
   ngOnInit() {
-  }
 
+  }
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+ setTimeout(() => {
+   this.bodyTemperature= this.randomNumber(35,37);
+   this.data.push(this.bodyTemperature);
+   this.label.push(this.date.getFullYear()+'/'+this.date.getUTCMonth()+'/'+this.date.getDate());
+   this.barChart.update();
+   console.log('Async operation has ended');
+   event.target.complete();
+ }, 4000);
+}
+
+
+randomNumber(min, max) {
+ return Math.floor(Math.random() * (max - min )) + min;
+}
+
+  ionViewWillEnter() {
+    this.bodyTemperature = this.route.snapshot.params.bodyTemperature;
+    this.data.push(this.bodyTemperature);
+    this.label.push(this.date.getFullYear()+'/'+this.date.getUTCMonth()+'/'+this.date.getDate());
+    this.barChartMethod();
+  }
+   //chart test
+   barChartMethod() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: this.label,
+        datasets: [{
+          label: '# Celsius',
+          data: this.data,
+          backgroundColor: [
+            'rgba(81, 119, 223, 1)',
+
+          ],
+          borderColor: [
+            'rgba(81, 119, 223, 1)',
+          ],
+          borderWidth: 1
+        }]
+      },
+     /*  options: {
+        scaleShowValues: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              autoSkip: false
+            }
+          }]
+        }
+      } */
+    });
+  }
 }
--- src/app/telemetry/respiratory-rate/respiratory-rate.page.ts
+++ src/app/telemetry/respiratory-rate/respiratory-rate.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable max-len */
+import { ActivatedRoute, Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-respiratory-rate',
@@ -7,9 +13,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RespiratoryRatePage implements OnInit {
 
-  constructor() { }
+  @ViewChild('barCanvas') private barCanvas: ElementRef;
+
+  public respiratoryRate: any;
+  barChart: any;
+  label: any [] =['2022/5/22', '2022/5/23', '2022/5/26', '2022/5/27', '2022/6/1','2022/6/2'];
+  data: any[]=[15, 24, 18, 22, 17, 27];
+  date= new Date();
+  constructor(  public router: Router,
+    private route: ActivatedRoute,
+    private storage: Storage
+    ) { Chart.register(...registerables);}
 
   ngOnInit() {
+
+  }
+  doRefresh(event) {
+       console.log('Begin async operation');
+
+    setTimeout(() => {
+      this.respiratoryRate= this.randomNumber(15,30);
+      this.data.push(this.respiratoryRate);
+      this.label.push(this.date.getFullYear()+'/'+this.date.getUTCMonth()+'/'+this.date.getDate());
+      this.barChart.update();
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 4000);
+  }
+  ionViewWillEnter() {
+    this.respiratoryRate = this.route.snapshot.params.respiratoryRate;
+    this.data.push(this.respiratoryRate);
+    this.label.push(this.date.getFullYear()+'/'+this.date.getUTCMonth()+'/'+this.date.getDate());
+    this.barChartMethod();
+    console.log(this.date);
+  }
+
+  randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min )) + min;
+  }
+
+   //chart test
+   barChartMethod() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: this.label,
+        datasets: [{
+          label: 'Per minute',
+          data: this.data,
+          backgroundColor: [
+            'rgba(86, 196, 69, 1)',
+
+          ],
+          borderColor: [
+            'rgba(86, 196, 69, 1)',
+          ],
+          borderWidth: 1
+        }]
+      },
+     /*  options: {
+        scaleShowValues: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              autoSkip: false
+            }
+          }]
+        }
+      } */
+    });
   }
 
 }
